12c16d2661b8d575a068b2a04f40b57816925707
Laboratory of NeuroGenetics <ark19@duke.edu>
2014-03-19 15:30:56 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'ff36c76bb50df96ec7b1d8523dafbb699f99e2ff7bbf737c9b284a90876ac3de8d4f5e1bd0b8000ebf5c734423dd25e095dfd9883aa2252ed2202fadd52d170d'
